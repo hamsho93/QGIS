3fbc3b7b38fa29883efb4efb17cec61a20567168
hamsho93 <hamshom@gmail.com>
2017-09-28 16:55:46 -0700

    Add files via upload

--- PycharmProjects/untitled/qgis.py
+++ PycharmProjects/untitled/qgis.py
@@ -6,11 +6,14 @@ import re
 #Name of the files from the statewide database
 file_name_1 = 'c001_g16_sov_data_by_g16_svprec.dbf'
 file_name_2 = 'c003_g16_sov_data_by_g16_svprec.dbf'
+file_name_3 = 'c005_g16_sov_data_by_g16_svprec.dbf'
+
 
 #Reads DBF file
 # df = DataFrame(iter(DBF('state_g16_sov_data_by_g16_svprec.dbf')))
 df1 = DataFrame(iter(DBF(file_name_1)))
 df2 = DataFrame(iter(DBF(file_name_2)))
+df3 = DataFrame(iter(DBF(file_name_3)))
 
 
 # #Transfers to csv file
@@ -32,21 +35,32 @@ def fip(file_name):
         return found
 
 
-y = fip(file_name_1)
-
-
+##LOCation
 
 #This function concatenates fip and the svprec
-#Adds the new column to the end of the existing DataFrame
+#Adds the new column "Svprec_Key" to the end of the existing DataFrame
+#Adds "FIPS" column
 def svprec_key(fip_key,dataframe):
 
-    dataframe.insert(1, "Svprec_Key", fip_key + dataframe['svprec'])
+    dataframe.insert(0, "FIPS", "6"+ fip(fip_key))
+    dataframe.insert(1, "Svprec_Key", "6" + fip(fip_key) + dataframe['svprec'])
 
 
+svprec_key(file_name_1, df1)
+svprec_key(file_name_3, df3)
 
 
+# Joins the dataframes together
+frames = [df1, df3]
+result = pd.DataFrame(pd.concat(frames))
 
-z = svprec_key(y, df1)
+
+#Adds Election and Type column into the final dataframe
+result.insert(0,"Election","g16")
+result.insert(1,"Type", "sov")
 
 
-print(df1.head())
+result.to_csv("Result.csv")
+
+
+#Next Step: Figure how to read in all the files into this program
